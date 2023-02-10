aad08b0f806a84a6fc1624f49505390826c3c195
Thukten Singye <thuktensingye2163@gmail.com>
2023-02-10 07:16:39 +0600

    handle choice and comparing if its match

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Button from '@material-ui/core/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SingleCard from './component/SingleCard';
 // make an array of card cause the card are constant that will never chaneg
 const cardImages = [
@@ -15,6 +15,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]) // used to store card state
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
   // shuffle card using sort methjod 
   const shuffleCards = ()=>{
     const shuffleCard = [...cardImages, ...cardImages]
@@ -26,7 +28,32 @@ function App() {
     setTurns(0)
 
   }
+  const handleChoice = (card)=>{
+    choiceOne ? setChoiceTwo(card): setChoiceOne(card)
+    // if (choiceOne.src == choiceTwo.src)
+  }
+  // now how to compare two selected item 
+  useEffect(()=>{
+    if (choiceOne && choiceTwo){ // true when both have an value
+      // now compare using src property
+      if (choiceOne.src === choiceTwo.src){
+        console.log("The selected item match ")
+        resetTurn()
+      }
+      else{
+        console.log("The item didnt match")
+        resetTurn()
+      }
+
+    }
+  },[choiceOne, choiceTwo])
   // console.log(cards, turns)
+  // reset the choice and increase the turn
+  const resetTurn = () =>{
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns (prevTurn => prevTurn+1)
+  }
   
   return (
     <div className="app">
@@ -34,9 +61,8 @@ function App() {
      <Button onClick={shuffleCards}>New Game</Button>
      <div className='card-grid'>
       {cards.map((card)=>(
-          <SingleCard key={card.id} card ={card}/>
+          <SingleCard key={card.id} card ={card} handleChoice ={handleChoice}/>
         ))}
-     
      </div>
     </div>
   );
--- src/component/SingleCard.js
+++ src/component/SingleCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./SingleCard.css"
 
-function SingleCard({card}) {
+function SingleCard({card, handleChoice}) {
+  const handleClick =()=>{
+    handleChoice(card)
+  }
+
   return (
     <div className='card'>
       <img className='front' src={card.src} alt="card front" />
-      <img className='back' src="./img/cover.png" alt="card back" />
+      <img className='back' onClick={handleClick} src="./img/cover.png" alt="card back" />
     </div>
   )
 }
